ce266e4b791396740fa841c1fb9980c05585878f
Rosyid <rosyidgrobogan@gmail.com>
2021-04-17 13:44:32 +0700

    + adding react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { getAllStudents } from './helpers/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    student: [],
+  };
+
+  componentDidMount() {
+    this.fetchStudent();
+  }
+
+  fetchStudent = () => {
+    getAllStudents().then((res) =>
+      res.json().then((students) => {
+        console.log(students);
+        this.setState({
+          // students: students
+          students,
+        });
+      })
+    );
+  };
+
+  //   {getAllStudents().then((res) =>
+  //   res.json().then((students) => {
+  //     console.log(students);
+  //   })
+  // )}
+  render() {
+    // const { students } = this.props;
+    const { students } = this.state;
+
+    if (students && students.length) {
+      return students.map((student, id) => {
+        return (
+          <div key={id}>
+            <h2>{student.studentId}</h2>
+            <p>{student.firstName}</p>
+            <p>{student.lastName}</p>
+            <p>{student.gender}</p>
+            <p>{student.email}</p>
+          </div>
+        );
+      });
+    }
+
+    return <h2>No Students found</h2>;
+  }
 }
 
 export default App;
