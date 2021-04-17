5c9964850bdf2ba81f41a515025e8f3dc1821b92
Rosyid <rosyidgrobogan@gmail.com>
2021-04-17 14:26:10 +0700

    + Ant Design Table Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import { getAllStudents } from './helpers/client';
+import { Table } from 'antd';
 
 class App extends Component {
   state = {
@@ -33,17 +34,43 @@ class App extends Component {
     const { students } = this.state;
 
     if (students && students.length) {
-      return students.map((student, id) => {
-        return (
-          <div key={id}>
-            <h2>{student.studentId}</h2>
-            <p>{student.firstName}</p>
-            <p>{student.lastName}</p>
-            <p>{student.gender}</p>
-            <p>{student.email}</p>
-          </div>
-        );
-      });
+      // return students.map((student, id) => {
+      //   return (
+      //     <div key={id}>
+      //       <h2>{student.studentId}</h2>
+      //       <p>{student.firstName}</p>
+      //       <p>{student.lastName}</p>
+      //       <p>{student.gender}</p>
+      //       <p>{student.email}</p>
+      //     </div>
+      //   );
+      // });
+      const columns = [
+        {
+          title: 'StudentId',
+          dataIndex: 'studentId',
+          key: 'studentId',
+        },
+        {
+          title: 'FirstName',
+          dataIndex: 'firstName',
+          key: 'firstName',
+        },
+        {
+          title: 'LastName',
+          dataIndex: 'lastName',
+          key: 'lastName',
+        },
+        {
+          title: 'Gender',
+          dataIndex: 'gender',
+          key: 'gender',
+        },
+      ];
+
+      return (
+        <Table dataSource={students} columns={columns} rowKey='studentId' />
+      );
     }
 
     return <h2>No Students found</h2>;
