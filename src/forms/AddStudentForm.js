400e7bd6359d7c5fb5125fa99d687dfb655c6ec7
Rosyid <rosyidgrobogan@gmail.com>
2021-05-20 16:59:12 +0700

    + Adding form text fields

--- src/forms/AddStudentForm.js
+++ src/forms/AddStudentForm.js
@@ -1,39 +1,88 @@
 import React, { Component } from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik } from 'formik';
+import { Input, Button } from 'antd';
 
+const inputButtomMargin = { marginBottom: '5px' };
 class AddStudentForm extends Component {
   render() {
     return (
-      <div>
-        <Formik
-          initialValues={{
-            firstName: '',
-            lastName: '',
-            email: '',
-          }}
-          onSubmit={async (values) => {
-            await new Promise((r) => setTimeout(r, 500));
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={(values) => {
+          const errors = {};
+
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
-          }}
-        >
-          <Form>
-            <label htmlFor='firstName'>First Name</label>
-            <Field id='firstName' name='firstName' placeholder='Jane' />
 
-            <label htmlFor='lastName'>Last Name</label>
-            <Field id='lastName' name='lastName' placeholder='Doe' />
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
 
-            <label htmlFor='email'>Email</label>
-            <Field
-              id='email'
-              name='email'
-              placeholder='jane@acme.com'
-              type='email'
+          errors,
+
+          touched,
+
+          handleChange,
+
+          handleBlur,
+
+          handleSubmit,
+
+          isSubmitting,
+
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Input
+              style={inputButtomMargin}
+              name='firstName'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.firstName}
+              placeholder='First Name'
             />
-            <button type='submit'>Submit</button>
-          </Form>
-        </Formik>
-      </div>
+            {errors.firstName && touched.firstName && errors.firstName}
+
+            <Input
+              style={{ marginBottom: '5px' }}
+              name='lastName'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.lastName}
+              placeholder='Last Name'
+            />
+            {errors.lastName && touched.lastName && errors.lastName}
+
+            <Input
+              style={inputButtomMargin}
+              name='email'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+              placeholder='Email'
+            />
+            {errors.email && touched.email && errors.email}
+
+            <Button type='submit' disabled={isSubmitting}>
+              Submit
+            </Button>
+          </form>
+        )}
+      </Formik>
     );
   }
 }
