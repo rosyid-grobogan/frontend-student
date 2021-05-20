f04ade27c8b40809f382aca9e1dfb49d46388349
Rosyid <rosyidgrobogan@gmail.com>
2021-05-21 06:50:30 +0700

    + Client Side POST

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,3 +1,12 @@
 import fetch from 'unfetch';
 
 export const getAllStudents = () => fetch('/students');
+
+export const addNewStudent = (student) =>
+  fetch('api/students', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(student),
+  });
